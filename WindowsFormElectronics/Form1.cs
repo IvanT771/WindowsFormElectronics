3e32e8238f19e2ffe4900adf22e0be7ab4b85c55
IvanT771 <cnyazev.ivan@yandex.ru>
2021-04-18 16:29:25 +0300

    Fix error

--- WindowsFormElectronics/Form1.cs
+++ WindowsFormElectronics/Form1.cs
@@ -51,19 +51,19 @@ namespace WindowsFormElectronics
         //Метод защиты ввода 
         private bool isProtect()
         {
-            if(richTextBox2.Text.Length <=0 || richTextBox3.Text.Length <= 0
+            if (richTextBox2.Text.Length <= 0 || richTextBox3.Text.Length <= 0
                || richTextBox4.Text.Length <= 0 || richTextBox5.Text.Length <= 0 || richTextBox6.Text.Length <= 0)
             {
-                MessageBox.Show("Поля ввода не должны быть путыми!", "Ошибка", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show("Поля ввода не должны быть путыми!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
-                return true;
+            return true;
         }
 
         //Метод обработки нажатия кнопки "Выполнить расчет"
-        private void button_Calculation(object obj,EventArgs e)
+        private void button_Calculation(object obj, EventArgs e)
         {
-            if (!isProtect()) { return;}
+            if (!isProtect()) { return; }
 
             double N = 0;   //Концентрация
             double T = 0;   //Температура
@@ -79,13 +79,14 @@ namespace WindowsFormElectronics
                 Double.TryParse(richTextBox4.Text, out Lx) &&
                 Double.TryParse(richTextBox5.Text, out Ly) &&
                 Double.TryParse(richTextBox6.Text, out Lz)
-                )) 
-                {
+                ))
+            {
                 MessageBox.Show("Данные введены некорректно!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            if (T < 0 || T > 1000){
+            if (T < 0 || T > 1000)
+            {
                 MessageBox.Show("Данные введены некорректно! Температура должна быть в диапазоне от 0К до 1000К", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
@@ -127,189 +128,56 @@ namespace WindowsFormElectronics
             richTextBox9.Text = minor.ToString("#.#####E+0");
             richTextBox8.Text = R.ToString("#.#####E+0");
         }
-        private void Form1_Resize2(object sender, EventArgs e)
-        {
-          //  panelLeft.Size = new Size(this.Size.Width / 2 - 20, panelLeft.Size.Height);
-          //  panelRight.Size = new Size(this.Size.Width / 2, panelRight.Size.Height);
-
-          ////  panelMaterial.Size = new Size(panelMaterial.Size.Width, (int)((panelLeft.Size.Height + panelMaterial.Size.Height) * 0.13f));
-          ////  panelRightTop.Size = panelMaterial.Size;
 
 
-          //  int width = (int)(panelMaterial.Size.Width / 4) - 8;
-          //  int height = (int)(panelMaterial.Size.Height) - 10;
-          //  buttonSi.Width = width;
-          //  buttonSi.Height = height;
-          //  buttonSi.Font = new Font(buttonSi.Font.FontFamily, width / 6, buttonSi.Font.Style);
-          //  buttonSi.Location = new Point(5, 5);
-
-          //  buttonGe.Width = width;
-          //  buttonGe.Height = height;
-          //  buttonGe.Font = buttonSi.Font;
-          //  buttonGe.Location = new Point(width + 10, 5);
-
-          //  buttonGaAs.Width = width;
-          //  buttonGaAs.Height = height;
-          //  buttonGaAs.Font = buttonSi.Font;
-          //  buttonGaAs.Location = new Point(width * 2 + 15, 5);
-
-          //  buttonInP.Width = width;
-          //  buttonInP.Height = height;
-          //  buttonInP.Font = buttonSi.Font;
-          //  buttonInP.Location = new Point(width * 3 + 20, 5);
-        }
-        //Изменение размера формы (временно откл.)
-        #region resizeForm
-        //Обработчик изменения размера формы
-        //private void Form1_Resize(object sender, EventArgs e)
-        //{
-        //    panelLeft.Size = new Size(this.Size.Width/2-20, panelLeft.Size.Height);
-        //    panelRight.Size = new Size(this.Size.Width/2, panelRight.Size.Height);
-
-        //    panelMaterial.Size = new Size(panelMaterial.Size.Width, (int)((panelLeft.Size.Height + panelMaterial.Size.Height)*0.13f));
-        //    panelRightTop.Size = panelMaterial.Size;
-
-
-        //    int width = (int)(panelMaterial.Size.Width/4) - 8;
-        //    int height = (int)(panelMaterial.Size.Height) - 10;
-        //    buttonSi.Width = width;
-        //    buttonSi.Height = height;
-        //    buttonSi.Font = new Font(buttonSi.Font.FontFamily,width/6, buttonSi.Font.Style);
-        //    buttonSi.Location = new Point(5,5);
-
-        //    buttonGe.Width = width;
-        //    buttonGe.Height = height;
-        //    buttonGe.Font = buttonSi.Font;
-        //    buttonGe.Location = new Point(width+10,5);
-
-        //    buttonGaAs.Width = width;
-        //    buttonGaAs.Height = height;
-        //    buttonGaAs.Font = buttonSi.Font;
-        //    buttonGaAs.Location = new Point(width*2+15,5);
-
-        //    buttonInP.Width = width;
-        //    buttonInP.Height = height;
-        //    buttonInP.Font = buttonSi.Font;
-        //    buttonInP.Location = new Point(width*3+20,5);
-
-        //    height = (int)(panelInput.Height/6) - 11;
-        //    width = (int)(panelInput.Width*0.4f);
-
-        //    richTextBox1.Width = width;
-        //    richTextBox1.Height = height;
-        //    richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, height / 2, richTextBox1.Font.Style); 
-        //    richTextBox1.Location = new Point(panelInput.Width - richTextBox1.Width,10);
-
-        //    richTextBox2.Width = width;
-        //    richTextBox2.Height = height;
-        //    richTextBox2.Font = richTextBox1.Font;
-        //    richTextBox2.Location = new Point(panelInput.Width - richTextBox1.Width, height+15);
-
-        //    richTextBox3.Width = width;
-        //    richTextBox3.Height = height;
-        //    richTextBox3.Font = richTextBox1.Font;
-        //    richTextBox3.Location = new Point(panelInput.Width - richTextBox1.Width, height*2+20);
-
-        //    richTextBox4.Width = width;
-        //    richTextBox4.Height = height;
-        //    richTextBox4.Font = richTextBox1.Font;
-        //    richTextBox4.Location = new Point(panelInput.Width - richTextBox1.Width, height*3+25);
-
-        //    richTextBox5.Width = width;
-        //    richTextBox5.Height = height;
-        //    richTextBox5.Font = richTextBox1.Font;
-        //    richTextBox5.Location = new Point(panelInput.Width - richTextBox1.Width, height*4+30);
-
-        //    richTextBox6.Width = width;
-        //    richTextBox6.Height = height;
-        //    richTextBox6.Font = richTextBox1.Font;
-        //    richTextBox6.Location = new Point(panelInput.Width - richTextBox1.Width, height*5+35);
-
-        //    width = (int)(panelInput.Width * 0.58f);
-
-        //    label3.Width = width;
-        //    label3.Height = height;
-        //    label3.Font = new Font(label3.Font.FontFamily, (height+width) / 23f, label3.Font.Style);
-        //    label3.Location = new Point(0, 10);
-
-        //    label4.Width = width;
-        //    label4.Height = height;
-        //    label4.Font = label3.Font;
-        //    label4.Location = new Point(0, height + 15);
-
-        //    label5.Width = width;
-        //    label5.Height = height;
-        //    label5.Font = label3.Font;
-        //    label5.Location = new Point(0, height * 2 + 20);
-
-        //    label6.Width = width;
-        //    label6.Height = height;
-        //    label6.Font = label3.Font;
-        //    label6.Location = new Point(0, height * 3 + 25);
-
-        //    label7.Width = width;
-        //    label7.Height = height;
-        //    label7.Font = label3.Font;
-        //    label7.Location = new Point(0, height * 4 + 30);
-
-        //    label8.Width = width;
-        //    label8.Height = height;
-        //    label8.Font = label3.Font;
-        //    label8.Location = new Point(0, height * 5 + 35);
-
-        //    richTextBox12.Size = richTextBox1.Size;
-        //    richTextBox12.Location = richTextBox1.Location;
-        //    richTextBox12.Font = richTextBox1.Font;
-
-        //    label14.Size = label3.Size;
-        //    label14.Location = label3.Location;
-        //    label14.Font = label3.Font;
-
-        //    richTextBox11.Size = richTextBox1.Size;
-        //    richTextBox11.Location = richTextBox2.Location;
-        //    richTextBox11.Font = richTextBox2.Font;
-
-        //    label13.Size = label4.Size;
-        //    label13.Location = label4.Location;
-        //    label13.Font = label4.Font;
-
-        //    richTextBox10.Size = richTextBox1.Size;
-        //    richTextBox10.Location = richTextBox3.Location;
-        //    richTextBox10.Font = richTextBox3.Font;
-
-        //    label12.Size = label5.Size;
-        //    label12.Location = label5.Location;
-        //    label12.Font = label5.Font;
-
-        //    richTextBox9.Size = richTextBox1.Size;
-        //    richTextBox9.Location = richTextBox4.Location;
-        //    richTextBox9.Font = richTextBox3.Font;
-
-        //    label11.Size = label5.Size;
-        //    label11.Location = label6.Location;
-        //    label11.Font = label5.Font;
-
-        //    richTextBox8.Size = richTextBox1.Size;
-        //    richTextBox8.Location = richTextBox5.Location;
-        //    richTextBox8.Font = richTextBox3.Font;
-
-        //    label10.Size = label5.Size;
-        //    label10.Location = label7.Location;
-        //    label10.Font = label5.Font;
-
-        //}
-        #endregion
 
         //Обработчик заполнения полей textBox 
         private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (!(e.KeyChar >= '0' && e.KeyChar <= '9'))
+            {
+                if (e.KeyChar == ',' || e.KeyChar == '.' || e.KeyChar == 'б' || e.KeyChar == 'ю' || e.KeyChar == '/')
+                {
+                    if (((RichTextBox)sender).Text.IndexOf(',') == -1 && ((RichTextBox)sender).Text.Length > 0 && ((RichTextBox)sender).Text.IndexOf('E') == -1)
+                    {
+                        e.KeyChar = ',';
+                        e.Handled = false;
+                        return;
+                    }
+                }
+                if (e.KeyChar == 'E' || e.KeyChar == 'e' || e.KeyChar == 'у' || e.KeyChar == 'У')
+                {
+                    if (((RichTextBox)sender).Text.IndexOf('E') == -1 && ((RichTextBox)sender).Text.Length > 0)
+                    {
+                        e.KeyChar = 'E';
+                        e.Handled = false;
+                        return;
+                    }
+                }
+                if (e.KeyChar == '-')
+                {
+                    if (((RichTextBox)sender).Text.Length > 0)
+                    {
+                        if (((RichTextBox)sender).Text[((RichTextBox)sender).Text.Length - 1] == 'E')
+                        {
+                            e.Handled = false;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                        return;
+                    }
+                }
+                e.Handled = true;
+            }
         }
 
         //Кнопка сброса
         private void button2_Click(object sender, EventArgs e)
         {
-           
+
             richTextBox2.Clear();
             richTextBox3.Clear();
             richTextBox4.Clear();
@@ -337,18 +205,49 @@ namespace WindowsFormElectronics
             if (isDonor)
             {
                 // N - тип
-                label4.Text = "Концентрация примеси Nd:(см:-3)";
-                label12.Text = "Nn:(см:-3)";
-                label11.Text = "Pn:(см:-3)";
+                label4.Text = "Концентрация примеси Nd (см^-3):";
+                label12.Text = "Nn (см^-3):";
+                label11.Text = "Pn (см^-3):";
             }
             else
             {
                 // P - тип
-                label4.Text = "Концентрация примеси Na:(см:-3)";
-                label12.Text = "Pp:(см:-3)";
-                label11.Text = "Np:(см:-3)";
+                label4.Text = "Концентрация примеси Na (см^-3):";
+                label12.Text = "Pp (см^-3):";
+                label11.Text = "Np (см^-3):";
             }
 
         }
+
+        //Button close application
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #region MoveForm
+        private bool isMove = false;
+        private Point currentPosition = new Point(0, 0);
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMove = true;
+            currentPosition = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMove = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMove) { return; }
+            Point tmp = new Point(this.Location.X, this.Location.Y);
+            tmp.X += e.X - currentPosition.X;
+            tmp.Y += e.Y - currentPosition.Y;
+            this.Location = tmp;
+
+        }
+        #endregion
     }
 }
